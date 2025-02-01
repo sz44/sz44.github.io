aff4807a79eccae73c20a3c2232253a729892363
sz44 <semion44@gmail.com>
2025-01-31 16:42:13 -0800

    can schema change help load images?

--- config.ts
+++ config.ts
@@ -7,6 +7,15 @@ import { glob, file } from 'astro/loaders';
 // 3. Define your collection(s)
 const projects = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/projects" }),
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    pubDate: z.coerce.date(),
+    image: z.object({
+      url: image(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
 });
 
 // 4. Export a single `collections` object to register your collection(s)
